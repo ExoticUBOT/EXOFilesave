5c30c9eb4d94bd3cd6c1f1f58b014b418427f46a
ExoticUBOT <91032497+ExoticUBOT@users.noreply.github.com>
2022-01-06 12:38:12 +0800

    Update config.py

--- config.py
+++ config.py
@@ -13,25 +13,30 @@ API_HASH = os.environ.get("API_HASH", "")
 
 #Your db channel Id
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
-#username channel
-CHANNEL = os.environ.get("CHANNEL", "")
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 
+#Database 
+DB_URI = os.environ.get("DATABASE_URL", "")
+
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Halo {firstname}\n\n\Anda harus berlangganan dengan channel / group kami untuk dapat menggunakan saya.")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
+
+#Force sub message 
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
@@ -45,7 +50,6 @@ ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
 
 LOG_FILE_NAME = "filesharingbot.txt"
-
 
 logging.basicConfig(
     level=logging.INFO,
